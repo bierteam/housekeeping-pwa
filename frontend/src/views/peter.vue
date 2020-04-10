bf4063f790fd356f985557254a43cfa24609aebc
BuildTools <unconfigured@null.spigotmc.org>
2020-04-10 20:41:09 +0200

    GrotereBlij

--- frontend/src/views/peter.vue
+++ frontend/src/views/peter.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="templates">
-    <h1>Peter is koning</h1>
+    <h1>Zoekmachine Links</h1>
     <div>
       <b-dropdown
         text="Knop"
@@ -8,9 +8,13 @@
         class="m-md-2"
         lazy= "true"
         >
+        <b-dropdown-text>Normaal</b-dropdown-text>
         <b-dropdown-item href="https://www.google.nl/" target="_blank">Google</b-dropdown-item>
-        <b-dropdown-text>of</b-dropdown-text>
         <b-dropdown-item href="https://duckduckgo.com/" target="_blank">DuckDuckGo</b-dropdown-item>
+        <b-dropdown-text>Porn</b-dropdown-text>
+        <b-dropdown-item href="https://pornhub.com/" target="_blank">Pornhub</b-dropdown-item>
+        <b-dropdown-item href="https://xvideos.com/" target="_blank">Xvideos</b-dropdown-item>
+
       </b-dropdown>
     </div>
   </div>
