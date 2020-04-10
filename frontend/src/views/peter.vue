a653cd6392861bbc1890f09bd1ebe1b33a63db80
BuildTools <unconfigured@null.spigotmc.org>
2020-04-10 20:50:56 +0200

    Bloopers

--- frontend/src/views/peter.vue
+++ frontend/src/views/peter.vue
@@ -11,9 +11,9 @@
         <b-dropdown-text>Normaal</b-dropdown-text>
         <b-dropdown-item href="https://www.google.nl/" target="_blank">Google</b-dropdown-item>
         <b-dropdown-item href="https://duckduckgo.com/" target="_blank">DuckDuckGo</b-dropdown-item>
-        <b-dropdown-text>Porn</b-dropdown-text>
-        <b-dropdown-item href="https://pornhub.com/" target="_blank">Pornhub</b-dropdown-item>
-        <b-dropdown-item href="https://xvideos.com/" target="_blank">Xvideos</b-dropdown-item>
+        <b-dropdown-text>Epische Links</b-dropdown-text>
+        <b-dropdown-item href="https://pornhub.com/" target="_blank">Hehe</b-dropdown-item>
+        <b-dropdown-item href="https://xvideos.com/" target="_blank">Hoho</b-dropdown-item>
 
       </b-dropdown>
     </div>
--- frontend/src/views/test.vue
+++ frontend/src/views/test.vue
@@ -1,15 +1,6 @@
 <template>
   <div class="templates">
     <h1>Dit is een test</h1>
-    <div>
-      <b-dropdown id="dropdown-1" text="Dropdown Button" class="m-md-2">
-        <b-dropdown-item>First Action</b-dropdown-item>
-        <b-dropdown-item>Second Action</b-dropdown-item>
-        <b-dropdown-item>Third Action</b-dropdown-item>
-        <b-dropdown-divider></b-dropdown-divider>
-        <b-dropdown-item active>Active action</b-dropdown-item>
-        <b-dropdown-item disabled>Disabled action</b-dropdown-item>
-      </b-dropdown>
-    </div>
+
   </div>
 </template>
