f5bb5e7e151a60880cb66cbde3ee0d552c666c7d
NvdLaan <34895069+NvdLaan@users.noreply.github.com>
2020-04-12 17:18:27 +0200

    Make tests run with npm test

--- backend/test/services/userservice.test.js
+++ backend/test/services/userservice.test.js
@@ -18,12 +18,12 @@ describe('UserService', () => {
     const token = await userService.createUserAsync({ username: 'test', password: 'test' })
 
     // Assert
-    expect(token).toMatch(/^[A-Za-z0-9-_=]+.[A-Za-z0-9-_=]+.?[A-Za-z0-9-_.+/=]*$/) // regex checks if it's a valid jwt  
+    expect(token).toMatch(/^[A-Za-z0-9-_=]+.[A-Za-z0-9-_=]+.?[A-Za-z0-9-_.+/=]*$/) // regex checks if it's a valid jwt
   })
 
   test('createUserAsync the returned JWT should contain the username and _id of the newly created user', async () => {
     // Arrange
-    const newUser = new User({ username: 'test', password: 'test', _id: 'fictional id'})
+    const newUser = new User({ username: 'test', password: 'test', _id: 'fictional id' })
     newUser.save = jest.fn().mockResolvedValue({ test: 'test' })
     const userService = new UserService(newUser)
 
@@ -31,14 +31,14 @@ describe('UserService', () => {
     const token = await userService.createUserAsync({ username: 'test', password: 'test' })
     const claims = jwt.verify(token, process.env.JWTSECRET)
 
-    //Assert
+    // Assert
     expect(claims.username).toBe(newUser.username)
     expect(claims._id).toBe(newUser._id)
   })
 
   test('signInUserAsync should return a valid JWT', async () => {
     // Arrange
-    const newUser = new User({ username: 'test', password: 'test', _id: 'fictional id'})
+    const newUser = new User({ username: 'test', password: 'test', _id: 'fictional id' })
     const mockingToken = await newUser.generateAuthToken()
     User.findByCredentials = jest.fn().mockResolvedValue(mockingToken)
     const userService = new UserService(newUser)
@@ -49,10 +49,10 @@ describe('UserService', () => {
     // Assert
     expect(token).toBe(mockingToken)
   })
-  
+
   test('signInUserAsync the returned JWT should contain the username and _id of the newly created user', async () => {
     // Arrange
-    const newUser = new User({ username: 'test', password: 'test', _id: 'fictional id'})
+    const newUser = new User({ username: 'test', password: 'test', _id: 'fictional id' })
     const mockingToken = await newUser.generateAuthToken()
     User.findByCredentials = jest.fn().mockResolvedValue(mockingToken)
     const userService = new UserService(newUser)
@@ -61,21 +61,23 @@ describe('UserService', () => {
     const token = await userService.signInUserAsync()
     const claims = jwt.verify(token, process.env.JWTSECRET)
 
-    //Assert
+    // Assert
     expect(claims.username).toBe(newUser.username)
     expect(claims._id).toBe(newUser._id)
   })
 
   test('signInUserAsync throws an error when no user is found', async () => {
     // Arrange
-    const newUser = new User({ username: 'test', password: 'test', _id: 'fictional id'})
+    const newUser = new User({ username: 'test', password: 'test', _id: 'fictional id' })
     User.findByCredentials = jest.fn().mockResolvedValue(null)
     const userService = new UserService(newUser)
 
     try {
+      // Act
       await userService.signInUserAsync()
       expect(true).toBe(false) // test failed
     } catch (error) {
+      // Assert
       expect(error.message).toBe('Invalid username or password')
     }
   })
