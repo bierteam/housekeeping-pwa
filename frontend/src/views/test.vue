0cad1600c321aa503852906dbca08e3a57f39f6b
Peter <Oosteromers@hotmail.com>
2020-04-17 22:39:42 +0200

    hh

--- frontend/src/views/test.vue
+++ frontend/src/views/test.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <h1>Kei gave goede test om te controleren of ik nu eindelijk mijn facking naam heb veranderd :D</h1>
+    <h1>Kei gave goede test om te connnntroleren of ik nu eindelijk mijn facking naam heb veranderd :D</h1>
   </div>
 </template>
